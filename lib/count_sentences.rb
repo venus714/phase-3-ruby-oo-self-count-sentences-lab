231b15ccb43f1a82125fa2ca60b2845e5947c262
Student <venus.kabera@student.moringaschool.com>
2023-02-11 20:12:30 +0300

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,18 @@
 require 'pry'
-
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if { |word| word.size < 2 }.size
   end
 end
